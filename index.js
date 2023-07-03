ed1b340cd0990836d769151c12473b8fd82a351f
Amandubey211 <109097090+Amandubey211@users.noreply.github.com>
2023-07-04 00:05:03 +0530

    Update index.js
    es module fix (dirname)

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import morgan from "morgan";
 import cors from "cors";
 import { config } from "dotenv";
 import path from "path"
+import {fileURLToPath} from "url;
 import authRouter from "./Routes/authRoute.js";
 import ProductRouter from "./Routes/productRoute.js";
 import CategoryRouter from "./Routes/categoryRoute.js";
@@ -13,7 +14,12 @@ const app = express();
 
 config({ path: "./config/config.env" });
 
+//database connection
+DBconnect();
 
+//esmodule fix
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 //middlewares
 app.use(express.static(path.join(__dirname,"./client/build")))
 app.use(morgan("dev"));
@@ -31,8 +37,6 @@ app.use("/api/admin/product", ProductRouter);
 
 app.use("/api/admin/category", CategoryRouter);
 
-//database connection
-DBconnect();
 
 app.use("*",function(req,res){
   res.sendFile(path.join(__dirname,"./client/build/index.html"))
